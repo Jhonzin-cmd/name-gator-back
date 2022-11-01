82bead1ed95ca854fe001078a010c0c889b2e213
Joao Gabriel <joao.gabriel@beresolution.com.br>
2022-11-01 17:59:57 -0300

    feat: add items mutation

---
+++ src/database.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    id: String(1),
+    name: 'Gabriel',
+    age: 22,
+    email: 'gabriel@email.com',
+    profession: 'dev',
+  },
+  {
+    id: String(2),
+    name: 'Carlos',
+    age: 20,
+    email: 'carlos@email.com',
+    profession: 'Entregador',
+  },
+  {
+    id: String(3),
+    name: 'Oliveira',
+    age: 25,
+    email: 'oliveira@email.com',
+    profession: 'padeiro',
+  },
+  {
+    id: String(4),
+    name: 'Ricardo',
+    age: 18,
+    email: 'ricardo@email.com',
+    profession: 'mkt',
+  },
+
+];
